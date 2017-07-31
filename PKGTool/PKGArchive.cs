9ffbb0bee9ac1598b4f2116b7b2f6dc65cf04b7a
thesupersonic16 <supersonic16bit@gmail.com>
2017-07-31 20:53:21 +1000

    Added Initial Files

---
+++ PKGTool/PKGArchive.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using HedgeLib.IO;
+
+namespace HedgeLib.Archives
+{
+    public class PKGArchive : Archive
+    {
+        public override void Load(Stream fileStream)
+        {
+            var reader = new ExtendedBinaryReader(fileStream, false);
+
+            reader.JumpAhead(4); // CRC32?
+            int fileCount = reader.ReadInt32();
+
+            var fileEntries = new List<FileEntry>();
+            for (int i = 0; i < fileCount; ++i)
+            {
+                var fileEntry = new FileEntry();
+                fileEntry.FileName = reader.ReadSignature(0x40).Replace("\0", "");
+                fileEntry.DataUncompressedSize = reader.ReadUInt32();
+                fileEntry.DataSize = reader.ReadUInt32();
+                fileEntry.DataOffset = reader.ReadUInt32();
+                fileEntry.Compressed = reader.ReadByte() == 1;
+                reader.JumpAhead(3); // Other Attributes?
+                fileEntries.Add(fileEntry);
+            }
+
+            foreach (var fileEntry in fileEntries)
+            {
+                reader.JumpTo(fileEntry.DataOffset);
+
+                byte[] data = null;
+                if (fileEntry.Compressed)
+                    data = ReadAndDecompress(reader);
+                else
+                    data = reader.ReadBytes((int)fileEntry.DataUncompressedSize);
+
+                // Adds the File
+                Data.Add(new ArchiveFile()
+                {
+                    Name = fileEntry.FileName,
+                    Data = data
+                });
+            }
+
+        }
+
+        public override void Save(Stream fileStream)
+        {
+            var writer = new ExtendedBinaryWriter(fileStream, false);
+
+            // TODO
+            writer.AddOffset("crc32"); // CRC32?
+            writer.Write(Data.Count);
+
+            var fileDatas = new List<byte[]>();
+            int i = 0;
+            foreach (ArchiveFile file in Data)
+            {
+                var fileNameBuffer = new char[0x40];
+                file.Name.CopyTo(0, fileNameBuffer, 0, file.Name.Length);
+                writer.Write(fileNameBuffer);
+
+                var fileData = file.Data;
+                writer.Write(fileData.Length);      // Uncompressed Size
+                writer.Write(fileData.Length);      // Compressed Size
+                writer.AddOffset("fileOffset" + i); // File Offset
+                writer.Write(0u);                   // Is Compressed 0 = FALSE, 1 = TRUE
+                fileDatas.Add(fileData);
+                ++i;
+            }
+
+            for (i = 0; i < fileDatas.Count; ++i)
+            {
+                writer.FillInOffset("fileOffset" + i);
+                writer.Write(fileDatas[i]);
+            }
+
+            // TODO
+            writer.FillInOffset("crc32", 0u);
+
+        }
+
+        public static byte[] ReadAndDecompress(ExtendedBinaryReader reader)
+        {
+            var stream = new MemoryStream();
+            uint decompressedSize = reader.ReadUInt32();
+            uint compressedSize = reader.ReadUInt32();
+            byte copyByte = reader.ReadByte();
+            reader.JumpAhead(3);
+            uint dataStartOffset = (uint)reader.BaseStream.Position;
+
+            while (stream.Position < decompressedSize)
+            {
+                byte b = reader.ReadByte();
+
+                if (b == copyByte)
+                {
+                    byte returnByte = reader.ReadByte();
+
+                    if (returnByte == copyByte)
+                    {
+                        stream.WriteByte(returnByte);
+                        continue;
+                    }
+
+                    if (returnByte >= copyByte)
+                        returnByte--;
+
+                    uint offset = (uint)stream.Position - returnByte;
+                    byte length = reader.ReadByte();
+                    uint currentPosition = (uint)stream.Position;
+
+                    stream.Position = offset;
+                    byte[] buffer = new byte[length];
+                    stream.Read(buffer, 0, length);
+                    stream.Position = currentPosition;
+                    stream.Write(buffer, 0, length);
+                }
+                else
+                    stream.WriteByte(b);
+            }
+            return stream.ToArray();
+        }
+
+        public class FileEntry
+        {
+            public string FileName;
+            public uint DataSize;
+            public uint DataUncompressedSize;
+            public uint DataOffset;
+            public bool Compressed;
+        }
+    }
+}
---
+++ PKGTool/Program.cs
@@ -0,0 +1,132 @@
+﻿using HedgeLib.Archives;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PKGTool
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(Application.ExecutablePath);
+            if (args.Length == 0)
+            {
+                Print($"{fileName} {{File Path / Directory Path}}");
+                return;
+            }
+            else if (new FileInfo(args[0]).Exists)
+            {
+                ExtractPKG(args[0]);
+            }
+            else if (new DirectoryInfo(args[0]).Exists)
+            {
+                RepackPKG(args[0]);
+            }
+            else
+            {
+                PrintError("File or Directory does not exist!");
+            }
+        }
+
+        public static void ExtractPKG(string filePath)
+        {
+            // Directory
+            string dirPath = Path.GetFileNameWithoutExtension(filePath);
+            dirPath = Path.Combine(Path.GetDirectoryName(filePath), dirPath);
+            if (!Directory.Exists(dirPath))
+                Directory.CreateDirectory(dirPath);
+
+            var archive = new PKGArchive();
+            
+            // Loads the archive
+            Print("Loading...");
+            archive.Load(filePath);
+            
+            // Extract the archive
+            Print("Extracting...");
+            // Extracts all the files with percent output
+            for (int i = 0; i < archive.Data.Count; ++i)
+            {
+                var data = archive.Data[i];
+                data.Extract(Path.Combine(dirPath, data.Name));
+                int percent = (int)(((float)i / archive.Data.Count) * 100f);
+                Print($"{percent}%\r", false);
+            }
+
+            Print("Done!");
+        }
+
+        public static void RepackPKG(string dirPath)
+        {
+            // Archive
+            var archive = new PKGArchive();
+            // Adds Files
+            Print("Adding Files...");
+            archive.AddDirectory(dirPath, false);
+            // Saving Archive
+            Print("Saving...");
+            archive.Save(dirPath + ".pkg");
+
+            Print("Done!");
+        }
+
+
+        // Other Functions
+        public static void Print(string text, bool newLine = true)
+        {
+            if (newLine)
+                Console.WriteLine(text);
+            else
+                Console.Write(text);
+        }
+
+        public static void PrintWarning(string text)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("[WARNING] {0}\n", text);
+            Console.ForegroundColor = color;
+        }
+
+        public static void PrintError(string text)
+        {
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[ERROR] {0}\n", text);
+            Console.ForegroundColor = color;
+        }
+
+        public static void PrintDebug(string text)
+        {
+            #if DEBUG
+            var color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("[DEBUG] {0}", text);
+            Console.ForegroundColor = color;
+            #endif
+        }
+
+        /// <summary>
+        /// Asks a Yes/No question to the user
+        /// </summary>
+        /// <param name="msg">The Question</param>
+        /// <returns>The Answer</returns>
+        public static bool YesNo(string msg)
+        {
+            Console.Write($"{msg} <Y/N>: ");
+
+            // Pause until the user enters either Y or N
+            var keyInfo = Console.ReadKey(true);
+            while (keyInfo.Key != ConsoleKey.Y && keyInfo.Key != ConsoleKey.N)
+                keyInfo = Console.ReadKey(true);
+
+            Console.WriteLine(keyInfo.Key == ConsoleKey.Y ? "Yes" : "No");
+            return (keyInfo.Key == ConsoleKey.Y);
+        }
+    }
+}
