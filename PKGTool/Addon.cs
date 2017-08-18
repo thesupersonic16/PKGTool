ef06eccdac3eb4a23e5bbe9e17034bfb237e4936
thesupersonic16 <supersonic16bit@gmail.com>
2017-08-18 19:52:22 +1000

    PKGTool can now be used as an addon

---
+++ PKGTool/Addon.cs
@@ -0,0 +1,17 @@
+﻿using HedgeLib.Archives;
+
+namespace PKGTool
+{
+    public class Addon : HedgeArchiveEditor.Addon
+    {
+        public override bool OnLoad()
+        {
+            var archive = new ArchiveAddon();
+            archive.ArchiveType = typeof(PKGArchive);
+            archive.ArchiveName = "Trails of Cold Steel Archive";
+            archive.FileExtensions.Add(".pkg");
+            Archives.Add(archive);
+            return true;
+        }
+    }
+}
